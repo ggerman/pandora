d824be7c4b03076dbe10bbe8cd45430918265ddd
Giménez Silva Germán Alberto <ggerman@gmail.com>
2015-05-04 23:45:53 +0000

    added slide operators and math

--- Base Conversion/compiling/03/operators.go
+++ Base Conversion/compiling/03/operators.go
@@ -3,5 +3,12 @@ package main
 import "fmt"
 
 func main() {
+  var n uint = 1
+  var result = 23 << n // LEFT SHIFT 00010111 -> 00101110
+  fmt.Println(result)
+  result = 7 >> n // RIGHT SHIFT 0111 -> 0011
+  fmt.Println(result)
 
+  fmt.Println("Pointer: ", &n, "->", *&n)
+  fmt.Println("Complex Númber: ", complex128(1))
 }
