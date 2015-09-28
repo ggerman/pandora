4b2e298cbc30f24a7a9edd23438f49b6e9cd3470
German Alberto Gimenez Silva <ggerman@gmail.com>
2015-09-28 15:24:48 -0300

    docker machine fast tutorial

---
+++ containers/gracias.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	people := [4]string{"Manuel Garcia", "Altoros", "EOSWeb", "Concurrecia"}
+	for _, val := range people {
+		fmt.Println("¡¡¡ Muchas gracias", val, " !!!")
+	}
+}
